b7a2bb69d9a85794c6856ac8010375f96de707ad
John Bryant <john.r.bryant@gmail.com>
2021-08-22 08:32:02 +1200

    finished rawdata_region

---
+++ rawdata_region/src/reg_internal_region.R
@@ -0,0 +1,27 @@
+
+## Create 'reg_immigration' data frame from 'pxweb_nonbirths'
+
+library(dplyr)
+library(assertr)
+library(readr)
+library(tidyr)
+
+pxweb_nonbirths <- readRDS("out/pxweb_nonbirths.rds")
+
+reg_internal_region <- pxweb_nonbirths %>%
+    filter(event %in% c("Internal in migration", "Internal out migration")) %>%
+    mutate(direction = factor(event,
+                              levels = c("Internal in migration", "Internal out migration"),
+                              labels = c("In", "Out"))) %>%
+    mutate(age = time - cohort - (triangle == "Upper")) %>%
+    mutate(is_valid_age = !is.na(age) & (age >= 0)) %>%
+    verify(is_valid_age | (count == 0L)) %>%
+    filter(is_valid_age) %>%
+    mutate(age = ifelse(age >= 100, "100+", age),
+           age = factor(age, levels = c(0:99, "100+"))) %>%
+    count(age, triangle, sex, region, time, direction, wt = count, name = "count") %>%
+    complete(age, triangle, sex, region, time, direction, fill = list(count = 0L)) %>%
+    arrange(direction, time, region, sex, age, triangle)
+
+write_csv(reg_internal_region,
+          file = "../data/reg_internal_region.csv")
